16d738537a240cd41ce9aa5b9e748ff4f236ae7e
Gabriel Rodrigues <145603180+gaabrielrodrigues@users.noreply.github.com>
2025-11-10 21:34:29 -0300

    update

---
+++ script.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById("btn")
+const res = document.getElementById("res")
+
+btn.addEventListener("click", () => {
+    fetch('http://ip-api.com/json/')
+        .then(response => response.json())
+        .then(data => {
+            res.innerHTML = `
+            <div class="res-card">
+                <p>Country: ${data.country}</p>
+                <p>Region: ${data.regionName}</p>
+                <p>City: ${data.city}</p>
+            </div>
+        `;
+        })
+        .catch(error => console.error('Error fetching data:', error));
+})
